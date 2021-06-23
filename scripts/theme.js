c330fd7657542688623dcc756d51b044431d8192
Osvaldo Valentin Garcia <co91.valentingarciaosvaldo@gmail.com>
2021-06-23 13:41:27 -0500

    Quitaando  lineas de sobra

--- scripts/theme.js
+++ scripts/theme.js
@@ -12,24 +12,10 @@ export default function changeTheme() {
   };
 
   document.addEventListener("click", (e) => {
-    if (e.target.matches(".theme-btn *")) {
-      e.preventDefault();
-
-      if (!document.body.classList.contains("dark-mode")) {
-        // Si no se tiene la clase dark-mode al hacer click
-        darkMode(); // Ejecutaremos la funcion dark-mode
-      } else {
-        ligthMode(); // De lo contrario se ejecutara la light mode
-      }
-
-      document.querySelector(".menu").classList.remove("active");
-    }
-
     if (e.target.matches(".theme-btn")) {
         e.preventDefault();
 
-      if (!document.body.classList.contains("dark-mode")) {
-        // Si no se tiene la clase dark-mode al hacer click
+      if (!document.body.classList.contains("dark-mode")) { // Si no se tiene la clase dark-mode al hacer click
         darkMode(); // Ejecutaremos la funcion dark-mode
       } else {
         ligthMode(); // De lo contrario se ejecutara la light mode
