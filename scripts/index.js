f819f158ce857c292a1ef4f8930dc2c8a571dac8
Osvaldo Valentin Garcia <co91.valentingarciaosvaldo@gmail.com>
2021-06-28 09:28:38 -0500

    Agregando ScrollReveal.js

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,19 @@ document.addEventListener("DOMContentLoaded", (e) => {
     menu(".menu-btn *");
     changeTheme();
 });
+
+ScrollReveal().reveal('#about', {
+    delay: 400,
+    duration: 500,
+    reset: true,
+});
+ScrollReveal().reveal('#projects', {
+    delay: 400,
+    duration: 500,
+    reset: true,
+});
+ScrollReveal().reveal('#services', {
+    delay: 400,
+    duration: 500,
+    reset: true,
+});
